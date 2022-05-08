2c6268f7715a30742a20855938e531eee52c07ed
Bin <bin.ngoctu@gmail.com>
2022-05-08 16:18:44 +0700

    Add get options for viewset

--- one/components/viewsets.py
+++ one/components/viewsets.py
@@ -4,11 +4,34 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
+from one.utils.data_processing import getattr_in_chain
+
 
 class BaseModelViewSet(ModelViewSet):
     """
     Custom ModelViewSet
     """
+
+    filter_fields = []
+
+    def get_options(self):
+        return "options", [
+            {
+                "key": item["key"],
+                "label": item["label"],
+                "data": [
+                    {
+                        "label": getattr_in_chain(obj, item["data_label"]),
+                        "value": getattr_in_chain(obj, item["data_value"]),
+                    }
+                    for obj in item["model"].objects.all()
+                ],
+            }
+            for item in self.filter_fields
+        ]
+
+    class Meta:
+        datatables_extra_json = ("get_options",)
 
     def get_model(self):
         """return model of class"""
